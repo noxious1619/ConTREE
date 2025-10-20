aad32d14c06fd08511fd9ee1aa9f0bfb47da782b
Dishant <shoonyam.108@gmail.com>
2025-10-20 16:46:30 +0530

    added libraries and setup

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold text-blue-400">
+        Tailwind CSS is working 🎉
+      </h1>
+    </div>
+  );
+}
+
+export default App;
