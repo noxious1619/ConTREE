e005f2fbbe535740868dccc265b98aa843feaaac
Dishant <shoonyam.108@gmail.com>
2025-10-21 08:36:04 +0530

    setup ready

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div className="flex h-screen items-center justify-center bg-gray-900 text-white">
-      <h1 className="text-4xl font-bold text-blue-400">
-        Tailwind CSS is working 🎉
-      </h1>
-    </div>
-  );
+    <>
+      <div className="text-center text-3xl text-blue-500"> 
+        Tailwind is working!
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
